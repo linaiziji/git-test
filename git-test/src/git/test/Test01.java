49e67e2748663b446a8ab8f8c46d92bc3ea2163f
lin <linaiziji9@163.com>
2020-03-22 19:49:58 +0800

    把本地工程添加到git

---
+++ git-test/src/git/test/Test01.java
@@ -0,0 +1,9 @@
+package git.test;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
